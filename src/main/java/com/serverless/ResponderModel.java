534b81c572a9b39bed6a52b442ca131ffbb6f6b6
Carlos Acevedo <cacevedod@devco.com.co>
2020-11-11 16:48:08 -0500

    ajustes de unitarias

--- src/main/java/com/serverless/ResponderModel.java
+++ src/main/java/com/serverless/ResponderModel.java
@@ -7,12 +7,15 @@ public class ResponderModel {
     private final String teamName = "devco";
 
     public String answer(String question) {
-        if ("".equals(question))
+        if ("".equals(question)){
             return teamName;
+        }
+
         Matcher sumMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(question);
         if (sumMatcher.matches()) {
             return String.valueOf(Integer.parseInt(sumMatcher.group(1)) + Integer.parseInt(sumMatcher.group(2)));
         }
+        
         return teamName;
     }
 
