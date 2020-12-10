fee92ec75de6e0f7cbfd801008ee2762e3519ceb
unknown <toto.huallullo@gmail.com>
2020-12-10 20:02:33 +0000

    pregunta 1 flash

--- src/main/java/com/serverless/ResponderModel.java
+++ src/main/java/com/serverless/ResponderModel.java
@@ -4,20 +4,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ResponderModel {
-    private final String teamName = "522";
+    private final String teamName = "Equipo 2";
+
+    //public String answer(String question) {
+      //  Matcher nameMatcher = Pattern.compile(".*what is your name").matcher(question);
+       // if (nameMatcher.matches()) {
+        //    return teamName;
+       // }
+
+       // Matcher sumMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(question);
+        //if (sumMatcher.matches()) {
+         //   return String.valueOf(Integer.parseInt(sumMatcher.group(1)) + Integer.parseInt(sumMatcher.group(2)));
+       // }
+        
+     //   return "";
+   // }
 
     public String answer(String question) {
-        Matcher nameMatcher = Pattern.compile(".*").matcher(question);
-        if (nameMatcher.matches()) {
-            return teamName;
+        String[] q = question.split(":");
+        String[] val = q[2].split(",");
+        if (Integer.parseInt(val[0].trim()) > Integer.parseInt(val[1].trim())){
+            return val[0].trim();
+        }else{
+            return val[1].trim();
         }
-
-        Matcher sumMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(question);
-        if (sumMatcher.matches()) {
-            return String.valueOf(Integer.parseInt(sumMatcher.group(1)) + Integer.parseInt(sumMatcher.group(2)));
-        }
-        
-        return "";
     }
 
 }
--- src/test/java/com/serverless/ResponderModelTest.java
+++ src/test/java/com/serverless/ResponderModelTest.java
@@ -9,10 +9,21 @@ public class ResponderModelTest {
     public void CuandopreguntePorElNombrerespondeEquipo2(){
         ResponderModel responderModel = new ResponderModel();
         //Arrange
-        String pregunta = "dae244e0: which of the following numbers is the largest: 522, 38";
+        String pregunta = "07a05250: what is your name";
         //Act
         String respuesta = responderModel.answer(pregunta);
         //Assert
-        assertThat(respuesta).isEqualTo("522");
+        assertThat(respuesta).isEqualTo("Equipo 2");
+    }
+
+    @Test
+    public void CuandopreguntePorQuienesMayorrespondeEquipo2(){
+        ResponderModel responderModel = new ResponderModel();
+        //Arrange
+        String pregunta = "6cb3ab00: which of the following numbers is the largest: 437, 85";
+        //Act
+        String respuesta = responderModel.answer(pregunta);
+        //Assert
+        assertThat(respuesta).isEqualTo("437");
     }
 }
